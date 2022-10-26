db1f8c391d0980fa57eb8e68510a40af8415dfe9
Snahal Kumar <77937488+snahal04@users.noreply.github.com>
2022-10-27 02:33:12 +0530

    Update ir_test_1.cpp

--- IR Sensor/ir_test_1.cpp
+++ IR Sensor/ir_test_1.cpp
@@ -1,20 +1,35 @@
 //ir sensor code
+/*
+ * created by Somya Sinha
+ * IR Sensor
+ *
+    IR Sensor Pins:
+        VCC: +5VDC
+        OUT : Output Pin as  - Pin11
+        GND: GND
+ */
+
+//IR sensor range is 2-7 cm.
+
+int irpin = 3;
+int led = 13;
+
 void setup()
 {
-  pinMode(13,OUTPUT);  // configuring pin 13 as output pin
-  pinMode(3,INPUT);    //configuring pin 3 as input pin
+  pinMode(led,OUTPUT);  // configuring pin 13 as output pin to glow led
+  pinMode(irpin,INPUT);    //configuring pin 3 as input pin to detect object
   Serial.begin(9600);  //baudrate
 }
 void loop()
 {
-  if(digitalRead(3)==LOW)
+  if(digitalRead(irpin)==LOW) //If object is not detected
   {
-    digitalWrite(13,HIGH);
+    digitalWrite(led,HIGH); //turn the led high
     delay(10);   //delay of 10 microseconds
   }
   else
   {
-     digitalWrite(13,LOW);
+     digitalWrite(led,LOW);
     delay(10);   //delay of 10 microseconds
     
   }
